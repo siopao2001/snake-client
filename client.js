5db73930f9f60e44b8a6cb08d5af7c66adc60d87
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-16 21:29:24 +0000

    made functions modular

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+conn.on('data', (data) => {
+  console.log(data);
+});
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+module.exports = {
+   connect
+}
--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-conn.on('data', (data) => {
-  console.log(data);
-});
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
