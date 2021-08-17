d55ca59dbf65cdbd7ff1e14b658fc3a32faddbbd
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-17 15:17:09 +0000

    Refactored and add WASD

--- client.js
+++ client.js
@@ -5,12 +5,12 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
-conn.on('connect', () => {
-  conn.write('Name: pfz');
-});
+ 
 conn.on('connect', () => {
   console.log('Successfully connected to game server');
+  conn.write("Name: PVL");
 });
+
 conn.on('data', (data) => {
   console.log(data);
 });
