d25c7ced2c51dfc0e8c1bd29952c50db79131e66
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-17 13:21:32 +0000

    stdin setup

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
 const { connect } = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key)
+  return stdin;
+  
+ }
+ )
+}
+
+const handleUserInput = function (key) {
+   if (key === '\u0003') {
+     process.exit();
+  }
+}
+
 console.log('Connecting ...');
 connect();
+setupInput();
